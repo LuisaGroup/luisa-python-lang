916c93d5f708705d368e4bbc37eb2c131b3e3610
Xiaochun Tong <xtong@uwaterloo.ca>
2025-06-16 05:36:09 -0400

    disable examples for now

--- examples/generic_vecadd.py
+++ examples/generic_vecadd.py
@@ -1,14 +1,14 @@
-from typing import Any, TypeVar
-import luisa_lang
-from luisa_lang import Buffer, dispatch_id, static_assert
+# from typing import Any, TypeVar
+# import luisa_lang
+# from luisa_lang import Buffer, dispatch_id, static_assert
 
-T = TypeVar("T", bound=Any)
+# T = TypeVar("T", bound=Any)
 
-@luisa_lang.kernel
-def vecadd(a: Buffer[T], b: Buffer[T], c: Buffer[T]):
-    static_assert(
-        type(a[0] + b[0]) == type(c[0]),
-        f"Type {type(a)} does not support addition.",
-    )
-    tid = dispatch_id().x
-    c[tid] = a[tid] + b[tid]
+# @luisa_lang.kernel
+# def vecadd(a: Buffer[T], b: Buffer[T], c: Buffer[T]):
+#     static_assert(
+#         type(a[0] + b[0]) == type(c[0]),
+#         f"Type {type(a)} does not support addition.",
+#     )
+#     tid = dispatch_id().x
+#     c[tid] = a[tid] + b[tid]
--- examples/vecadd.py
+++ examples/vecadd.py
@@ -1,9 +1,9 @@
-import luisa_lang
-from luisa_lang import Buffer, dispatch_id
-from dataclasses import dataclass
+# import luisa_lang
+# from luisa_lang import Buffer, dispatch_id
+# from dataclasses import dataclass
 
 
-@luisa_lang.kernel
-def vecadd(a: Buffer[float], b: Buffer[float], c: Buffer[float]):
-    tid = dispatch_id().x
-    c[tid] = a[tid] + b[tid]
+# @luisa_lang.kernel
+# def vecadd(a: Buffer[float], b: Buffer[float], c: Buffer[float]):
+#     tid = dispatch_id().x
+#     c[tid] = a[tid] + b[tid]
